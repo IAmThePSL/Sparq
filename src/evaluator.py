e737ecadc2bf5a20d368b52c4ec0eab58074f175
DaPSL <145846855+IAmThePSL@users.noreply.github.com>
2024-12-20 23:18:01 +0100

    Update evaluator.py

--- src/evaluator.py
+++ src/evaluator.py
@@ -1,1 +1,19 @@
-# Evaluates the AST (interpreter logic)
+# evaluator.py: Evaluates the AST (interpreter logic)
+class Evaluator:
+    def __init__(self):
+        self.variables = {}  # store variables
+
+    def evaluate(self, ast):
+        if ast[0] == "PRINT":
+            expr = ast[1]
+            if expr[0] == "IDENTIFIER":
+                value = self.variables.get(expr[1], None)
+                if value is None:
+                    raise Exception(f"Undefined variable: {expr[1]}")
+                print(value)
+            elif expr[0] in ["NUMBER", "STRING"]:
+                print(expr[1])
+            else:
+                raise Exception("Unsupported print argument")
+        else:
+            raise Exception(f"Unknown AST node: {ast}")
